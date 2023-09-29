63ef92cf4cf54b6b2da7c86ac3890a09ae2d49df
shravanbhoyar1 <shravanbhoyar3507>
2023-09-29 18:14:10 +0530

    backgrownd img added in home page

--- src/view/home/home.js
+++ src/view/home/home.js
@@ -1,13 +1,13 @@
 import "./home.css";
 import Navbar from "../../component/navbar/navbar";
 import Footer from "../../component/footer/footer";
-
+import ImgBack from './back.jpeg'
 export default function Home()
 {
     return (
         <div className="container">
          <Navbar/>  
-         <h1>Home</h1>
+         <img src={ ImgBack } className="back-img"/>
          <Footer/>
         </div>
     );
